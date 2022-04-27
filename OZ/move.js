a6a63314b6873b1db3e2fa0f8bf3795ca0446f30
Oscar Zaldana <oscar.zaldana@yahoo.com>
2022-04-27 10:33:40 -0700

    show pattern
    Added a clickable text that shows the hidden buttons

--- OZ/move.js
+++ OZ/move.js
@@ -33,3 +33,47 @@ function getColors(){
 
 createColorSequence();
 print();
+
+
+//bottom portion is connecting the top array to the figures
+
+var hidColor1 = document.getElementById("hiddenAnswer1");
+var hidColor2 = document.getElementById("hiddenAnswer2");
+var hidColor3 = document.getElementById("hiddenAnswer3");
+var hidColor4 = document.getElementById("hiddenAnswer4");
+
+function setHiddenColor(hiddenColor, num){
+    if(colorSequence[num] == 0){
+        hiddenColor.style.backgroundColor = "red";
+    }
+    else if(colorSequence[num] == 1){
+        hiddenColor.style.backgroundColor = "yellow";
+    }
+    else if(colorSequence[num] == 2){
+        hiddenColor.style.backgroundColor = "orange";
+    }
+    else if(colorSequence[num] == 3){
+        hiddenColor.style.backgroundColor = "green";
+    }
+    else if(colorSequence[num] == 4){
+        hiddenColor.style.backgroundColor = "pink";
+    }
+    else if(colorSequence[num] == 5){
+        hiddenColor.style.backgroundColor = "blue";
+    }
+    else if(colorSequence[num] == 6){
+        hiddenColor.style.backgroundColor = "purple";
+    }
+    else{
+        hiddenColor.style.backgroundColor = "brown";
+    }    
+}
+
+//this button is temperary just ment to show that the answer can be hidden
+document.getElementById("demo").onclick = function() {pressed()};
+function pressed(){
+    setHiddenColor(hidColor1, 0);
+    setHiddenColor(hidColor2, 1);
+    setHiddenColor(hidColor3, 2);
+    setHiddenColor(hidColor4, 3);
+}
